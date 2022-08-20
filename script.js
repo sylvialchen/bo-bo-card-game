ce5a4e192cce6070f635c89c197f212b13f245ce
Sylvia Chen <sylvialchen33@gmail.com>
2022-08-19 22:34:10 -0400

    first run of game logic

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 console.log("connected!")
 // 1. Pull in cards from API
-// 2. Create Game Logic
+// 2. Convert J-A to numbers
+// 3. Create Game Logic
+
+scoreCards = (x,y) => {
+    if (x === y) {
+        return ["pair x", x + 100];
+    } else if ((x + y) >= 10) {
+        let score = 10 * (x + y - 10) - 1;
+        return [`artificial ${x + y - 10}`, score];
+    } else if ((x + y) < 10) {
+        let score = 10 * (x + y)
+        return [`natural ${x + y}`, score];
+    }
+}
+
+let hand1 = scoreCards(5,5)
+let hand2 = scoreCards(2,2)
+
+console.log(hand1)
+compareHands = (hand1,hand2) => {
+    if (hand1[1] > hand2[1]) {
+        console.log("winner hand1");
+    } else if(hand1[1] < hand2[1]) {
+        console.log("winner hand2");
+    }
+}
+compareHands (hand1,hand2);
